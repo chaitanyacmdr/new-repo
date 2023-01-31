b4dd873b2a0001c971439f28e7c49935c10eda4a
chaitanyacmdr <chaitanyakrishnacmdr@gmail.com>
2023-01-31 15:54:20 +0530

    development branch is modified

---
+++ application.java
@@ -0,0 +1,9 @@
+public class Simple{
+public static void main(String args[]){
+System.out.println("hello javatpoint");
+class A{  
+static public void main(String... args){  
+System.out.println("hello java4");  
+}  
+}; 
+}}
